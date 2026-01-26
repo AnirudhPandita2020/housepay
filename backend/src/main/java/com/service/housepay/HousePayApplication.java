3cacf49685298d889bd5a433caf5d5ea6848fa1d
Anirudh Pandita <kppkanu@gmail.com>
2026-01-26 23:06:16 +0530

    Initial Commit

---
+++ backend/src/main/java/com/service/housepay/HousePayApplication.java
@@ -0,0 +1,13 @@
+package com.service.housepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousePayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousePayApplication.class, args);
+    }
+
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            Hello house pay
+        </>
+    )
+}
+
+export default App;
